7a52c4538f5ebc4730005c2f14010eea7a71ea0a
Vitaliy Guz <vitaliy.guz@gmail.com>
2016-07-07 12:07:22 +0300

    x-access-token

--- server.js
+++ server.js
@@ -141,8 +141,8 @@ router.route('/users')
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     next();
 });
 
